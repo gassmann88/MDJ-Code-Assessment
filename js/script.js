96456e6529dec1d735869793c52af884bc9f349b
Jim Gassmann <jim@webhomes.com>
2016-01-25 15:44:33 -0800

    Added sliding image and added keyframes for animation

--- js/script.js
+++ js/script.js
@@ -2,4 +2,13 @@ $(document).ready(function(){
 	
 	console.log("Document is ready");
 
+	$('button').on('click', function(){
+
+		$('.element').addClass('active');
+
+		setTimeout(function(){
+			$('.element').removeClass('active');
+		}, 5000)
+	});
+
 });
